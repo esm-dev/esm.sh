48dc65eb58064e6fc2b1d94a74ff98f95b0af541
X <i@jex.me>
2021-03-15 23:45:55 +0800

    chore: improve build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
-goos="linux"
-goarch="amd64"
+mmdb_china_ip_list_tag="20210308"
+dataUrl="https://github.com/alecthw/mmdb_china_ip_list/releases/download/$mmdb_china_ip_list_tag/china_ip_list.mmdb"
+saveAs="$(dirname $0)/../assets/china_ip_list.mmdb"
 
-read -p "split China traffic (y/n) ? " chinaip
-read -p "please enter the deploy GOOS(default is '$goos'): " val
-if [ "$val" != "" ]; then
-    goos="$val"
-fi
-read -p "please enter the deploy GOARCH(default is '$goarch'): " val
-if [ "$val" != "" ]; then
-    goarch="$val"
-fi
+read -p "split China traffic (y/n) ? " split_china_traffic
+read -p "please enter the deploy GOOS(default is 'linux'): " goos
+read -p "please enter the deploy GOARCH(default is 'amd64'): " goarch
 
-if [ "$chinaip" == "y" ]; then
-    echo "--- inject mmdb_china_ip_list..."
-    go run $(dirname $0)/prebuild.go $(dirname $0)
+if [ "$split_china_traffic" == "y" ]; then
+  echo "--- building china_ip_list.mmdb..."
+  if [ ! -f "$saveAs" ]; then
+    curl --fail --location --progress-bar --output "$saveAs" "$dataUrl"
     if [ "$?" != "0" ]; then
-        exit
+      exit
     fi
+  fi
+else
+  if [ -f "$saveAs" ]; then
+    rm "$saveAs"
+  fi
 fi
 
-echo "--- building(${goos}_$goarch)..."
+if [ "$goos" == "" ]; then
+  goos="linux"
+fi
+if [ "$goarch" == "" ]; then
+  goarch="amd64"
+fi
 export GOOS=$goos
 export GOARCH=$goarch
+echo "--- building(${goos}_$goarch)..."
 go build -o $(dirname $0)/esmd $(dirname $0)/../main.go
