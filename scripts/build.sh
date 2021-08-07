e952fd9c107a12ca3a514895a0018025aa50c6e1
ije <git@iamje.com>
2021-08-07 13:25:36 +0800

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 # mmdb_china_ip_list: https://github.com/alecthw/mmdb_china_ip_list
-mmdb_china_ip_list_tag="20210621"
+mmdb_china_ip_list_tag="20210803"
 dataUrl="https://github.com/alecthw/mmdb_china_ip_list/releases/download/${mmdb_china_ip_list_tag}/china_ip_list.mmdb"
 saveAs="$(dirname $0)/../embed/china_ip_list.mmdb"
 cacheTo="/tmp/china_ip_list.${mmdb_china_ip_list_tag}.mmdb"
 
-read -p "split China traffic? y/N " split_china_traffic
 read -p "build GOOS (default is 'linux'): " goos
 read -p "build GOARCH (default is 'amd64'): " goarch
+read -p "split China traffic? y/N " split_china_traffic
+
+if [ "$goos" == "" ]; then
+  goos="linux"
+fi
+
+if [ "$goarch" == "" ]; then
+  goarch="amd64"
+fi
 
 if [ "$split_china_traffic" == "y" ]; then
   if [ ! -f "$cacheTo" ]; then
@@ -25,13 +33,7 @@ else
   fi
 fi
 
-if [ "$goos" == "" ]; then
-  goos="linux"
-fi
-if [ "$goarch" == "" ]; then
-  goarch="amd64"
-fi
+echo "--- building(${goos}_$goarch)..."
 export GOOS=$goos
 export GOARCH=$goarch
-echo "--- building(${goos}_$goarch)..."
 go build -o $(dirname $0)/esmd $(dirname $0)/../main.go
