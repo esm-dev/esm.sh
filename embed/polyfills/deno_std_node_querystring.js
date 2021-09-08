cf22f676dfe42d3e8aacf545dcc95a1715ed7868
v1rtl <pilll.pl22@gmail.com>
2021-09-08 16:14:23 +0300

    add comments for bundling

--- embed/polyfills/deno_std_node_querystring.js
+++ embed/polyfills/deno_std_node_querystring.js
@@ -1,92 +1,98 @@
+/* deno mod bundle
+ * entry: deno.land/std/node/querystring.ts
+ * version: 0.106.0
+ *
+ *   $ git clone https://github.com/denoland/deno_std
+ *   $ cd deno_std/node
+ *   $ esbuild querystring.ts --target=esnext --format=esm --bundle --outfile=deno_std_node_querystring.js
+ */
+
 // querystring.ts
-var hexTable = new Array(256);
+var hexTable = new Array(256)
 for (let i = 0; i < 256; ++i) {
-  hexTable[i] = "%" + ((i < 16 ? "0" : "") + i.toString(16)).toUpperCase();
+  hexTable[i] = '%' + ((i < 16 ? '0' : '') + i.toString(16)).toUpperCase()
 }
-function parse(str, sep = "&", eq = "=", { decodeURIComponent: decodeURIComponent2 = unescape, maxKeys = 1e3 } = {}) {
-  const entries = str.split(sep).map((entry) => entry.split(eq).map(decodeURIComponent2));
-  const final = {};
-  let i = 0;
+function parse(str, sep = '&', eq = '=', { decodeURIComponent: decodeURIComponent2 = unescape, maxKeys = 1e3 } = {}) {
+  const entries = str.split(sep).map((entry) => entry.split(eq).map(decodeURIComponent2))
+  const final = {}
+  let i = 0
   while (true) {
-    if (Object.keys(final).length === maxKeys && !!maxKeys || !entries[i]) {
-      break;
+    if ((Object.keys(final).length === maxKeys && !!maxKeys) || !entries[i]) {
+      break
     }
-    const [key, val] = entries[i];
+    const [key, val] = entries[i]
     if (final[key]) {
       if (Array.isArray(final[key])) {
-        final[key].push(val);
+        final[key].push(val)
       } else {
-        final[key] = [final[key], val];
+        final[key] = [final[key], val]
       }
     } else {
-      final[key] = val;
+      final[key] = val
     }
-    i++;
+    i++
   }
-  return final;
+  return final
 }
 function encodeStr(str, noEscapeTable, hexTable2) {
-  const len = str.length;
-  if (len === 0)
-    return "";
-  let out = "";
-  let lastPos = 0;
+  const len = str.length
+  if (len === 0) return ''
+  let out = ''
+  let lastPos = 0
   for (let i = 0; i < len; i++) {
-    let c = str.charCodeAt(i);
+    let c = str.charCodeAt(i)
     if (c < 128) {
-      if (noEscapeTable[c] === 1)
-        continue;
-      if (lastPos < i)
-        out += str.slice(lastPos, i);
-      lastPos = i + 1;
-      out += hexTable2[c];
-      continue;
+      if (noEscapeTable[c] === 1) continue
+      if (lastPos < i) out += str.slice(lastPos, i)
+      lastPos = i + 1
+      out += hexTable2[c]
+      continue
     }
-    if (lastPos < i)
-      out += str.slice(lastPos, i);
+    if (lastPos < i) out += str.slice(lastPos, i)
     if (c < 2048) {
-      lastPos = i + 1;
-      out += hexTable2[192 | c >> 6] + hexTable2[128 | c & 63];
-      continue;
+      lastPos = i + 1
+      out += hexTable2[192 | (c >> 6)] + hexTable2[128 | (c & 63)]
+      continue
     }
     if (c < 55296 || c >= 57344) {
-      lastPos = i + 1;
-      out += hexTable2[224 | c >> 12] + hexTable2[128 | c >> 6 & 63] + hexTable2[128 | c & 63];
-      continue;
+      lastPos = i + 1
+      out += hexTable2[224 | (c >> 12)] + hexTable2[128 | ((c >> 6) & 63)] + hexTable2[128 | (c & 63)]
+      continue
     }
-    ++i;
-    if (i >= len)
-      throw new Deno.errors.InvalidData("invalid URI");
-    const c2 = str.charCodeAt(i) & 1023;
-    lastPos = i + 1;
-    c = 65536 + ((c & 1023) << 10 | c2);
-    out += hexTable2[240 | c >> 18] + hexTable2[128 | c >> 12 & 63] + hexTable2[128 | c >> 6 & 63] + hexTable2[128 | c & 63];
+    ++i
+    if (i >= len) throw new Deno.errors.InvalidData('invalid URI')
+    const c2 = str.charCodeAt(i) & 1023
+    lastPos = i + 1
+    c = 65536 + (((c & 1023) << 10) | c2)
+    out +=
+      hexTable2[240 | (c >> 18)] +
+      hexTable2[128 | ((c >> 12) & 63)] +
+      hexTable2[128 | ((c >> 6) & 63)] +
+      hexTable2[128 | (c & 63)]
   }
-  if (lastPos === 0)
-    return str;
-  if (lastPos < len)
-    return out + str.slice(lastPos);
-  return out;
+  if (lastPos === 0) return str
+  if (lastPos < len) return out + str.slice(lastPos)
+  return out
 }
-function stringify(obj, sep = "&", eq = "=", { encodeURIComponent: encodeURIComponent2 = escape } = {}) {
-  const final = [];
+function stringify(obj, sep = '&', eq = '=', { encodeURIComponent: encodeURIComponent2 = escape } = {}) {
+  const final = []
   for (const entry of Object.entries(obj)) {
     if (Array.isArray(entry[1])) {
       for (const val of entry[1]) {
-        final.push(encodeURIComponent2(entry[0]) + eq + encodeURIComponent2(val));
+        final.push(encodeURIComponent2(entry[0]) + eq + encodeURIComponent2(val))
       }
-    } else if (typeof entry[1] !== "object" && entry[1] !== void 0) {
-      final.push(entry.map(encodeURIComponent2).join(eq));
+    } else if (typeof entry[1] !== 'object' && entry[1] !== void 0) {
+      final.push(entry.map(encodeURIComponent2).join(eq))
     } else {
-      final.push(encodeURIComponent2(entry[0]) + eq);
+      final.push(encodeURIComponent2(entry[0]) + eq)
     }
   }
-  return final.join(sep);
+  return final.join(sep)
 }
-var decode = parse;
-var encode = stringify;
-var unescape = decodeURIComponent;
-var escape = encodeURIComponent;
+var decode = parse
+var encode = stringify
+var unescape = decodeURIComponent
+var escape = encodeURIComponent
 var querystring_default = {
   parse,
   encodeStr,
@@ -96,15 +102,5 @@ var querystring_default = {
   encode,
   unescape,
   escape
-};
-export {
-  decode,
-  querystring_default as default,
-  encode,
-  encodeStr,
-  escape,
-  hexTable,
-  parse,
-  stringify,
-  unescape
-};
+}
+export { decode, querystring_default as default, encode, encodeStr, escape, hexTable, parse, stringify, unescape }
--- embed/polyfills/deno_std_node_timers.js
+++ embed/polyfills/deno_std_node_timers.js
@@ -1,10 +1,19 @@
+/* deno mod bundle
+ * entry: deno.land/std/node/timers.ts
+ * version: 0.106.0
+ *
+ *   $ git clone https://github.com/denoland/deno_std
+ *   $ cd deno_std/node
+ *   $ esbuild timers.ts --target=esnext --format=esm --bundle --outfile=deno_std_node_timers.js
+ */
+
 // timers.ts
-var setTimeout = globalThis.setTimeout;
-var clearTimeout = globalThis.clearTimeout;
-var setInterval = globalThis.setInterval;
-var clearInterval = globalThis.clearInterval;
-var setImmediate = (cb, ...args) => globalThis.setTimeout(cb, 0, ...args);
-var clearImmediate = globalThis.clearTimeout;
+var setTimeout = globalThis.setTimeout
+var clearTimeout = globalThis.clearTimeout
+var setInterval = globalThis.setInterval
+var clearInterval = globalThis.clearInterval
+var setImmediate = (cb, ...args) => globalThis.setTimeout(cb, 0, ...args)
+var clearImmediate = globalThis.clearTimeout
 var timers_default = {
   setTimeout,
   clearTimeout,
@@ -12,13 +21,5 @@ var timers_default = {
   clearInterval,
   setImmediate,
   clearImmediate
-};
-export {
-  clearImmediate,
-  clearInterval,
-  clearTimeout,
-  timers_default as default,
-  setImmediate,
-  setInterval,
-  setTimeout
-};
+}
+export { clearImmediate, clearInterval, clearTimeout, timers_default as default, setImmediate, setInterval, setTimeout }
