2c5d5ea85674553ec3ffadf2ebad102afdb459ce
X <i@jex.me>
2021-09-12 01:09:26 +0800

    Update deno testings

--- test/postcss.test.ts
+++ test/postcss.test.ts
@@ -1,11 +1,10 @@
 import postcss from 'http://localhost:8080/postcss'
 import autoprefixer from 'http://localhost:8080/autoprefixer'
 
-Deno.test("check postcss wth autoprefixer plugin", async () => {
+Deno.test('check postcss wth autoprefixer plugin', async () => {
 	const { css } = await postcss([autoprefixer]).process(`
-  backdrop-filter: blur(5px);
-  user-select: none;
-`).async()
-
+		backdrop-filter: blur(5px);
+		user-select: none;
+	`).async()
 	console.log(css)
 })
--- test/react.test.tsx
+++ test/react.test.tsx
@@ -1,7 +1,7 @@
 import React from 'http://localhost:8080/react'
 import { renderToString } from 'http://localhost:8080/react-dom/server'
 
-Deno.test("check react server rendering", async () => {
+Deno.test('check react server rendering', async () => {
 	const html = renderToString(<h1>Hi :)</h1>)
 	console.log(html)
 })
