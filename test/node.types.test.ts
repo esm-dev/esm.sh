8b883db5dfd19a77f0a4d81c85ec2c1d3740e094
X <i@jex.me>
2021-09-14 09:58:29 +0800

    Update deno testing

--- test/node.types.test.ts
+++ test/node.types.test.ts
@@ -2,7 +2,7 @@ import { generate } from 'http://localhost:8080/astring'
 import { getURLParams } from 'http://localhost:8080/@tinyhttp/url'
 import { assert } from 'https://deno.land/std@0.106.0/testing/asserts.ts'
 
-Deno.test('check marked wth safeLoadFront parser', async () => {
+Deno.test('check modules are using nodejs builtin modules', async () => {
 	assert(typeof generate === 'function')
 	assert(typeof getURLParams === 'function')
 })
---
+++ test/typescript.test.ts
@@ -0,0 +1,5 @@
+import * as TS from 'http://localhost:8080/typescript'
+
+Deno.test('check offical typescript', async () => {
+	console.log(TS)
+})
