daa0a1d7bac8e4266f5ca98ba5d83de49c94cc28
X <i@jex.me>
2021-09-15 10:39:33 +0800

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-# mmdb_china_ip_list: https://github.com/alecthw/mmdb_china_ip_list
-mmdb_china_ip_list_tag="202108181710"
-dataUrl="https://github.com/alecthw/mmdb_china_ip_list/releases/download/${mmdb_china_ip_list_tag}/Country.mmdb"
-saveAs="$(dirname $0)/../embed/china_ip_list.mmdb"
-cacheTo="/tmp/china_ip_list.${mmdb_china_ip_list_tag}.mmdb"
-read -p "split China traffic? y/N " split_china_traffic
-if [ "$split_china_traffic" == "y" ]; then
-  if [ ! -f "$cacheTo" ]; then
-    echo "--- download china_ip_list.mmdb..."
-    curl --fail --location --progress-bar --output "$cacheTo" "$dataUrl"
-    if [ "$?" != "0" ]; then
-      exit
-    fi
-  fi
-  cp -f $cacheTo $saveAs
-else
-  if [ -f "$saveAs" ]; then
-    rm "$saveAs"
-  fi
-fi
-
 read -p "build GOOS (default is 'linux'): " goos
 read -p "build GOARCH (default is 'amd64'): " goarch
 if [ "$goos" == "" ]; then
