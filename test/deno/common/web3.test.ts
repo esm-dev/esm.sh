4de45ac76c1e99878a1cc432f7d6b47aed57d41a
X <i@jex.me>
2021-10-27 00:30:04 +0800

    Add web3 testing

---
+++ test/deno/common/web3.test.ts
@@ -0,0 +1,6 @@
+import Web3 from 'http://localhost:8080/web3'
+import { assert } from 'https://deno.land/std@0.106.0/testing/asserts.ts'
+
+Deno.test('check modules are using nodejs builtin modules', async () => {
+	assert(typeof Web3 === 'function')
+})
