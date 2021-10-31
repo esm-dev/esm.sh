4028b4d9421dcea0f62f4be66c8bda731cf6b853
X <i@jex.me>
2021-10-31 13:42:53 +0800

    Update playground preset files

--- server/embed/playground/preset.js
+++ server/embed/playground/preset.js
@@ -3,14 +3,21 @@ export default {
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width" />
+  <link rel="stylesheet" href="./style.css" />
 </head>
 <body>
   <div id="root"></div>
   <script type="module" src="./app.jsx"></script>
 </body>
 </html>`,
+  'style.css': `h1 {
+  font-family: Arial, Helvetica, sans-serif;
+}`,
   'app.jsx': `import React from '${location.origin}/react'
 import ReactDom from '${location.origin}/react-dom'
   
-ReactDom.render(<p>Hello World!</p>, document.getElementById('root'))`
+ReactDom.render(
+  <p>Hello World!</p>,
+  document.getElementById('root')
+)`
 }
