242dd9245a9caebd4087964e0fc5cc6795a863e4
X <i@jex.me>
2021-11-01 00:33:54 +0800

    Improve node perf_hooks polyfill

--- server/embed/polyfills/node_perf_hooks.js
+++ server/embed/polyfills/node_perf_hooks.js
@@ -4,6 +4,35 @@ function notImplemented(name) {
   throw new Error(`[esm.sh] pref_hooks: '${name}' is not implemented`)
 }
 
+export const performance = window.performance
+export const PerformanceObserver = window.PerformanceObserver
+export const PerformanceEntry = window.PerformanceEntry
+export const PerformanceObserverEntryList = window.PerformanceObserverEntryList
+
+export class PerformanceNodeTiming extends PerformanceEntry {
+  constructor() {
+    notImplemented('PerformanceNodeTiming')
+  }
+}
+
+export class Histogram {
+  constructor() {
+    notImplemented('Histogram')
+  }
+}
+
+export class IntervalHistogram extends Histogram {
+  constructor() {
+    notImplemented('IntervalHistogram')
+  }
+}
+
+export class RecordableHistogram extends Histogram {
+  constructor() {
+    notImplemented('RecordableHistogram')
+  }
+}
+
 export function createHistogram() {
   notImplemented('createHistogram')
 }
@@ -13,6 +42,14 @@ export function monitorEventLoopDelay() {
 }
 
 export default {
+  performance,
+  PerformanceEntry,
+  PerformanceNodeTiming,
+  PerformanceObserver,
+  PerformanceObserverEntryList,
+  Histogram,
+  IntervalHistogram,
+  RecordableHistogram,
   createHistogram,
   monitorEventLoopDelay
 }
