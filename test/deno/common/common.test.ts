c297d853eaaa7e0ebbf47d513b668842758bd87b
X <i@jex.me>
2021-11-02 02:52:11 +0800

    Add testing for #197

--- test/deno/common/common.test.ts
+++ test/deno/common/common.test.ts
@@ -1,8 +1,10 @@
 import { generate } from 'http://localhost:8080/astring'
 import { getURLParams } from 'http://localhost:8080/@tinyhttp/url'
+import { Sha256 } from 'http://localhost:8080/@aws-crypto/sha256-browser'
 import { assert } from 'https://deno.land/std@0.106.0/testing/asserts.ts'
 
 Deno.test('check modules are using nodejs builtin modules', async () => {
 	assert(typeof generate === 'function')
 	assert(typeof getURLParams === 'function')
+	assert(typeof Sha256 === 'function')
 })
