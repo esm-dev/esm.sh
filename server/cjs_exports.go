3ab837e0707cce3fa22055f8aef55eeaff2e6443
X <i@jex.me>
2021-11-11 13:55:27 +0800

    Improve `parseCJSModuleExports` function

--- server/cjs_exports.go
+++ server/cjs_exports.go
@@ -2,6 +2,9 @@ package server
 
 import (
 	"encoding/json"
+	"io/ioutil"
+	"path"
+	"strings"
 )
 
 type cjsExportsResult struct {
@@ -10,6 +13,28 @@ type cjsExportsResult struct {
 }
 
 func parseCJSModuleExports(buildDir string, importPath string, nodeEnv string) (ret cjsExportsResult, err error) {
+	if strings.HasSuffix(importPath, ".json") {
+		var data []byte
+		data, err = ioutil.ReadFile(path.Join(buildDir, importPath))
+		if err != nil {
+			return
+		}
+
+		var m map[string]interface{}
+		if json.Unmarshal(data, &m) == nil {
+			var exports []string
+			var i int
+			exports = make([]string, len(m))
+			for key := range m {
+				exports[i] = key
+				i++
+			}
+			ret = cjsExportsResult{Exports: exports}
+		}
+
+		return
+	}
+
 	data := invokeNodeService("parseCjsExports", map[string]interface{}{
 		"buildDir":   buildDir,
 		"importPath": importPath,
