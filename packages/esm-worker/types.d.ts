19a1089b0c99563c3d9db2ccd3b44bc588fad90e
X <i@jex.me>
2021-11-18 13:00:44 +0800

    WIP

--- packages/esm-worker/types.d.ts
+++ packages/esm-worker/types.d.ts
@@ -1,12 +1,27 @@
 type Worker = {
-	(req: Request): Response
+	(req: Request): Response | Promise<Response>
 }
 
-interface AppFileSystem {
+type AppFileSystem = {
 	readFile(): Promise<Uint8Array>
 }
 
-interface AppStorage { }
+
+type KVPutOptions = {
+	expiration?: number,
+	expirationTtl?: number,
+	metadata?: any
+}
+
+type AppStorage = {
+	get(key: string, options?: { cacheTtl?: number }): Promise<string | null>
+	get(key: string, options: { type: 'text', cacheTtl?: number }): Promise<string | null>
+	get<T = any>(key: string, options: { type: 'json', cacheTtl?: number }): Promise<T | null>
+	get(key: string, options: { type: 'arrayBuffer', cacheTtl?: number }): Promise<ArrayBuffer | null>
+	get(key: string, options: { type: 'stream', cacheTtl?: number }): Promise<ReadableStream | null>
+	put(key: string, value: string | ArrayBuffer | ArrayBuffer, options?: KVPutOptions): Promise<void>
+	delete(key: string): Promise<void>
+}
 
 export type Options = {
 	appFileSystem: AppFileSystem,
@@ -17,4 +32,4 @@ export type Options = {
 	isDev: boolean
 }
 
-export const createESMWorker: (options: Options) => void
+export const createESMWorker: (options: Options) => Worker
