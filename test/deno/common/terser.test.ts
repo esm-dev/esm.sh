d9cd722b397dd84331ed0f9b2a3224d078372040
X <i@jex.me>
2022-01-21 23:43:36 +0800

    Clean up

--- test/deno/common/terser.test.ts
+++ test/deno/common/terser.test.ts
@@ -2,7 +2,7 @@ import { minify } from 'http://localhost:8080/terser'
 import { assert } from 'https://deno.land/std@0.119.0/testing/asserts.ts'
 
 Deno.test('check terser minifiy', async () => {
-	var code = "function add(first, second) { return first + second; }";
+	var code = 'function add(first, second) { return first + second; }';
 	var result = await minify(code, { sourceMap: true });
 	assert(result.code === 'function add(n,d){return n+d}')
 	assert(JSON.parse(String(result.map)).names?.length === 3)
