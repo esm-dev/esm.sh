f1277a9535113dc052c2d4679e5d9bc6730c4e63
X <i@jex.me>
2022-02-05 22:57:34 +0800

    Update deno.land/std/node to 0.125.0

--- server/consts.go
+++ server/consts.go
@@ -5,7 +5,7 @@ const VERSION = 64
 
 const (
 	pkgCacheTimeout    = 10 * 60 // 10 minutes
-	denoStdNodeVersion = "0.122.0"
+	denoStdNodeVersion = "0.125.0"
 	nodejsMinVersion   = 16
 	nodejsLatestLTS    = "16.13.1"
 	nodeTypesVersion   = "16.11.9"
