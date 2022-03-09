df5328990413be57973ca549e93214e219ed99b5
X <i@jex.me>
2022-03-09 16:12:08 +0800

    Upgrade deno node polyfill to 0.128.0

--- server/consts.go
+++ server/consts.go
@@ -5,7 +5,7 @@ const VERSION = 67
 
 const (
 	pkgCacheTimeout    = 10 * 60 // 10 minutes
-	denoStdNodeVersion = "0.125.0"
+	denoStdNodeVersion = "0.128.0"
 	nodejsMinVersion   = 16
 	nodejsLatestLTS    = "16.14.0"
 	nodeTypesVersion   = "16.11.25"
