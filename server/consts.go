a739cd2b4ec15ca2d68f537b9775ed9eae0181fb
X <i@jex.me>
2022-04-04 15:19:41 +0800

    Upgrade deno std to 0.133.0 (close #298)

--- server/consts.go
+++ server/consts.go
@@ -5,7 +5,7 @@ const VERSION = 74
 
 const (
 	pkgCacheTimeout    = 10 * 60 // 10 minutes
-	denoStdNodeVersion = "0.130.0"
+	denoStdNodeVersion = "0.133.0"
 	nodejsMinVersion   = 16
 	nodejsLatestLTS    = "16.14.0"
 	nodeTypesVersion   = "16.11.25"
