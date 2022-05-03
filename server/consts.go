a8f1f83c3a3f8171969a80c587879d18cbe63f9c
X <i@jex.me>
2022-05-03 13:56:39 +0800

    Update nodejs requires

--- server/consts.go
+++ server/consts.go
@@ -6,6 +6,6 @@ const VERSION = 78
 const (
 	pkgCacheTimeout  = 10 * 60 // 10 minutes
 	nodejsMinVersion = 16
-	nodejsLatestLTS  = "16.14.0"
-	nodeTypesVersion = "16.11.25"
+	nodejsLatestLTS  = "16.15.0"
+	nodeTypesVersion = "16.11.33"
 )
