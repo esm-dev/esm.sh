3cdf14c4ef03b6e070a3b7fadf8c73fa491eea6c
X <i@jex.me>
2022-05-22 07:16:07 +0800

    v80

--- server/consts.go
+++ server/consts.go
@@ -2,7 +2,7 @@ package server
 
 const (
 	// esm.sh build version
-	VERSION          = 79
+	VERSION          = 80
 	nodejsMinVersion = 16
 	nodejsLatestLTS  = "16.15.0"
 	nodeTypesVersion = "16.11.33"
