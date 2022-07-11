cfc5ec24da2b6554366f902a14c42cd5aa397055
X <i@jex.me>
2022-07-12 07:15:01 +0800

    Fix exnternal testing

--- test/deno/external/preact.test.tsx
+++ test/deno/external/preact.test.tsx
@@ -1,8 +1,8 @@
 import { assert } from "https://deno.land/std@0.145.0/testing/asserts.ts";
 
-import { h } from "http://localhost:8080/preact@10.7.2";
-import render from "http://localhost:8080/preact-render-to-string@5.2.0?deps=preact@10.7.2";
-import useSWR from "http://localhost:8080/swr@1.3.0?alias=react:preact/compat&deps=preact@10.7.2";
+import { h } from "preact";
+import render from "preact-render-to-string";
+import useSWR from "swr";
 
 Deno.test("preact-swr(external)", async () => {
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
