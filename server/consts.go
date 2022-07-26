0cd31adeb7a8624e6edc069ce874743e914274ca
X <i@jex.me>
2022-07-27 02:10:54 +0800

    Upgrade pre-requirement

--- server/consts.go
+++ server/consts.go
@@ -4,6 +4,6 @@ const (
 	// esm.sh build version
 	VERSION          = 87
 	nodejsMinVersion = 16
-	nodejsLatestLTS  = "16.15.0"
-	nodeTypesVersion = "16.11.33"
+	nodejsLatestLTS  = "16.16.0"
+	nodeTypesVersion = "16.11.45"
 )
