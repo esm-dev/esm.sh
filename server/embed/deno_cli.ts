a36b3f120c633cef9d82137acfeb15d10bdf9f5c
X <i@jex.me>
2022-08-02 16:17:39 +0800

    Improve deno CLI script

--- server/embed/deno_cli.ts
+++ server/embed/deno_cli.ts
@@ -13,8 +13,140 @@ export type Package = {
 
 const VERSION = "v{VERSION}";
 
-async function add(args: string[]) {
+async function add(args: string[], options: string[]) {
   const pkgs = await Promise.all(args.map(fetchPkgInfo));
+  const importMap = await loadImportMap();
+
+  await Promise.all(pkgs.map((pkg) => addPkgToImportMap(pkg, importMap)));
+  await saveImportMap(importMap);
+  console.log(
+    `Added ${pkgs.length} packages to %cimport_map.json`,
+    "color:blue",
+  );
+  if (pkgs.length > 0) {
+    console.log(
+      pkgs.map((pkg, index) => {
+        const tab = index === pkgs.length - 1 ? "└─" : "├─";
+        return `${tab} ${pkg.name}@${pkg.version}`;
+      }).join("\n"),
+    );
+  }
+}
+
+async function remove(args: string[], options: string[]) {
+  const importMap = await loadImportMap();
+  const toRemove = args.filter((name) => name in importMap.imports);
+  for (const name of toRemove) {
+    Reflect.deleteProperty(importMap.imports, name);
+    Reflect.deleteProperty(importMap.imports, name + "/");
+    Reflect.deleteProperty(importMap.scopes, name);
+    Reflect.deleteProperty(importMap.scopes, name + "/");
+  }
+  await saveImportMap(importMap);
+  console.log(`Removed ${toRemove.length} packages`);
+  if (toRemove.length > 0) {
+    console.log(
+      toRemove.map((name, index) => {
+        const tab = index === toRemove.length - 1 ? "└─" : "├─";
+        return `${tab} ${name}`;
+      }).join("\n"),
+    );
+  }
+}
+
+async function upgrade(args: string[], options: string[]) {
+  const importMap = await loadImportMap();
+  const lastest = options.includes("--latest");
+  const toUpgrade =
+    (args.length === 0
+      ? Object.keys(importMap.imports).filter((name) =>
+        importMap.imports[name].startsWith("https://esm.sh/")
+      ).map((name) => {
+        let version: string;
+        if (lastest) {
+          version = "latest";
+        } else {
+          const url = importMap.imports[name];
+          const [, v] = url.match(/@(\d+\.\d+\.\d+(-[a-z0-9\-\.]+)?)/)!;
+          if (!v.includes("-")) {
+            version = v.split(".").slice(0, 2).join(".");
+          } else {
+            version = v;
+          }
+        }
+        return `${name}@${version}`;
+      })
+      : args.filter((name) =>
+        name in importMap.imports ||
+        name.slice(0, name.lastIndexOf("@")) in importMap.imports
+      ).map((name) => {
+        let version: string;
+        if ((name.startsWith("@") ? name.slice(1) : name).includes("@")) {
+          const a = name.split("@");
+          version = a.pop()!;
+          name = a.join("@");
+        } else if (lastest) {
+          version = "latest";
+        } else {
+          const url = importMap.imports[name] ??
+            importMap.imports[name.slice(0, name.lastIndexOf("@"))];
+          const [, v] = url.match(/@(\d+\.\d+\.\d+(-[a-z0-9\-\.]+)?)/)!;
+          if (!v.includes("-")) {
+            version = v.split(".").slice(0, 2).join(".");
+          } else {
+            version = v;
+          }
+        }
+        return `${name}@${version}`;
+      }));
+  const pkgs = await Promise.all(toUpgrade.map(fetchPkgInfo));
+  const upgraded: Package[] = [];
+
+  for (const pkg of pkgs) {
+    if (await addPkgToImportMap(pkg, importMap)) {
+      upgraded.push(pkg);
+    }
+  }
+
+  await saveImportMap(importMap);
+  console.log(`Upgraded ${upgraded.length} packages`);
+  if (upgraded.length > 0) {
+    console.log(
+      upgraded.map((pkg, index) => {
+        const tab = index === pkgs.length - 1 ? "└─" : "├─";
+        return `${tab} ${pkg.name}@${pkg.version}`;
+      }).join("\n"),
+    );
+  }
+}
+
+const cache = new Map<string, Package>();
+async function fetchPkgInfo(name: string): Promise<Package> {
+  if (cache.has(name)) {
+    return Promise.resolve(cache.get(name)!);
+  }
+
+  const res = await fetch(`https://esm.sh/${name}/package.json`);
+  if (res.status === 404) {
+    console.error(`%cerror%c: Package "${name}" not found`, "color:red", "");
+    Deno.exit(1);
+  }
+  if (!res.ok) {
+    throw new Error(
+      `Failed to fetch "${name}": ${res.status} ${res.statusText}`,
+    );
+  }
+
+  const pkg = await res.json();
+  if (!pkg.name || !pkg.version) {
+    throw new Error(`Invalid package.json of "${name}"`);
+  }
+
+  cache.set(name, pkg);
+  return pkg;
+}
+
+async function loadImportMap(): Promise<ImportMap> {
   const importMap: ImportMap = { imports: {}, scopes: {} };
   try {
     const raw = (await Deno.readTextFile("import_map.json")).trim();
@@ -32,38 +164,10 @@ async function add(args: string[]) {
       throw err;
     }
   }
+  return importMap;
+}
 
-  for (const pkg of pkgs) {
-    const { name, version, dependencies } = pkg;
-    const url = new URL(`https://esm.sh/${name}@${version}`);
-    url.searchParams.set("external", "*");
-    url.searchParams.set("pin", VERSION);
-    url.searchParams.sort();
-    importMap.imports[name] = url.href;
-    if (
-      typeof pkg.exports === "object" &&
-      Object.keys(pkg.exports).some((key) =>
-        key.length >= 3 && key.startsWith("./")
-      )
-    ) {
-      importMap.imports[name + "/"] = `${url.href}&path=/`;
-    }
-    if (pkg) {
-      if (dependencies) {
-        importMap.scopes[name] = {};
-        for (const [depName, depVersion] of Object.entries(dependencies)) {
-          const dep = `${depName}@${depVersion}`;
-          const depPkg = await fetchPkgInfo(dep);
-          const depUrl = new URL(
-            `https://esm.sh/${depPkg.name}@${depPkg.version}`,
-          );
-          depUrl.searchParams.set("pin", VERSION);
-          importMap.scopes[name][depName] = depUrl.href;
-        }
-      }
-    }
-  }
-
+async function saveImportMap(importMap: ImportMap): Promise<void> {
   // clean up
   for (const importName in importMap.imports) {
     for (const [scopeName, scope] of Object.entries(importMap.scopes)) {
@@ -76,51 +180,68 @@ async function add(args: string[]) {
     }
   }
 
+  // sort
   const sortedImports = sortImports(importMap.imports);
   const sortedScopes = Object.fromEntries(
     Object.entries(importMap.scopes).sort(sortByKey).map((
       [key, scope],
     ) => [key, sortImports(scope)]),
   );
+
+  // write
   await Deno.writeTextFile(
     "import_map.json",
     JSON.stringify({ imports: sortedImports, scopes: sortedScopes }, null, 2),
   );
-  console.log(`Added ${pkgs.length} packages to import map.`);
-  console.log(
-    pkgs.map((pkg, index) => {
-      const tab = index === pkgs.length - 1 ? "└─" : "├─";
-      return `${tab} ${pkg.name}@${pkg.version}`;
-    }).join("\n"),
-  );
 }
 
-async function remove(args: string[]) {
-  console.log("todos: remove command");
+async function addPkgToImportMap(
+  pkg: Package,
+  importMap: ImportMap,
+): Promise<boolean> {
+  const [pkgUrl, pkgSubUri] = getPkgUrl(pkg);
+  if (importMap.imports[pkg.name] === pkgUrl) {
+    return false;
+  }
+  importMap.imports[pkg.name] = pkgUrl;
+  if (pkgSubUri) {
+    importMap.imports[pkg.name + "/"] = pkgSubUri;
+  } else {
+    Reflect.deleteProperty(importMap.imports, pkg.name + "/");
+  }
+  if (pkg.dependencies) {
+    importMap.scopes[pkg.name] = {};
+    if (pkgSubUri) {
+      importMap.scopes[pkg.name + "/"] = {};
+    }
+    for (const [depName, depVersion] of Object.entries(pkg.dependencies)) {
+      const dep = `${depName}@${depVersion}`;
+      const depPkg = await fetchPkgInfo(dep);
+      const depUrl = new URL(
+        `https://esm.sh/${depPkg.name}@${depPkg.version}`,
+      );
+      depUrl.searchParams.set("pin", VERSION);
+      importMap.scopes[pkg.name][depName] = depUrl.href;
+      if (pkgSubUri) {
+        importMap.scopes[pkg.name + "/"][depName] = depUrl.href;
+      }
+    }
+  }
+  return true;
 }
 
-async function upgrade(args: string[]) {
-  console.log("todos: upgrade command");
-}
-
-const cache = new Map<string, Package>();
-async function fetchPkgInfo(name: string): Promise<Package> {
-  if (cache.has(name)) {
-    return Promise.resolve(cache.get(name)!);
+function getPkgUrl(pkg: Package): [string, string | null] {
+  const { name, version, exports } = pkg;
+  const hasSubModules = typeof exports === "object" &&
+    Object.keys(exports).some((key) => key.length >= 3 && key.startsWith("./"));
+  const url = new URL(`https://esm.sh/${name}@${version}`);
+  url.searchParams.set("external", "*");
+  url.searchParams.set("pin", VERSION);
+  url.searchParams.sort();
+  if (hasSubModules) {
+    return [url.href, `${url.href}&path=/`];
   }
-
-  const res = await fetch(`https://esm.sh/${name}/package.json`);
-  if (!res.ok) {
-    throw new Error(`Failed to fetch "${name}"`);
-  }
-
-  const pkg = await res.json();
-  if (!pkg.name || !pkg.version) {
-    throw new Error(`Invalid package.json of "${name}"`);
-  }
-
-  cache.set(name, pkg);
-  return pkg;
+  return [url.href, null];
 }
 
 function sortImports(imports: Record<string, string>) {
@@ -142,6 +263,7 @@ function sortByKey(a: [string, unknown], b: [string, unknown]) {
 }
 
 if (import.meta.main) {
+  const start = performance.now();
   const [command, ...args] = Deno.args;
   const commands = {
     add,
@@ -154,5 +276,13 @@ if (import.meta.main) {
     Deno.exit(1);
   }
 
-  await commands[command as keyof typeof commands](args);
+  try {
+    await commands[command as keyof typeof commands](
+      args.filter((arg) => !arg.startsWith("-")),
+      args.filter((arg) => arg.startsWith("-")),
+    );
+    console.log(`✨ Done in ${(performance.now() - start).toFixed(2)}ms`);
+  } catch (error) {
+    throw error;
+  }
 }
