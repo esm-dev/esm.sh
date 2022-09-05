2ead0ca04e6b9f8ef55394fb68d0833b924a556c
Luca Casonato <lucacasonato@yahoo.com>
2022-09-05 14:45:47 +0200

    Downgrade deno/std to 0.153.0 (#416)

--- server/consts.go
+++ server/consts.go
@@ -4,7 +4,7 @@ const (
 	// esm.sh build version
 	VERSION          = 93
 	nodejsMinVersion = 16
-	denoStdVersion   = "0.154.0"
+	denoStdVersion   = "0.153.0"
 	nodejsLatestLTS  = "16.16.0"
 	nodeTypesVersion = "16.11.45"
 )
