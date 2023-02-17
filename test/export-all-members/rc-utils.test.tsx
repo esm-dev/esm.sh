ebef808ed9684966e657424d997048ca8e4ba028
Justinidlerz <zqc.sunny@gmail.com>
2023-02-17 14:50:24 +0800

    Add test for export all members

---
+++ test/export-all-members/rc-utils.test.tsx
@@ -0,0 +1,10 @@
+import { assertExists } from "https://deno.land/std@0.170.0/testing/asserts.ts";
+
+import * as dynamicCSS from "http://localhost:8080/rc-util@5.27.2/es/Dom/dynamicCSS.js";
+
+Deno.test("Export all members when the package is not a standard ES module", async () => {
+    assertExists(dynamicCSS.updateCSS);
+    assertExists(dynamicCSS.injectCSS);
+    assertExists(dynamicCSS.removeCSS);
+    assertExists(dynamicCSS.clearContainerCache);
+});
