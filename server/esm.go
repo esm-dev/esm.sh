278605f3d84b9e86f2729126b4107dfde5f5b11e
X <i@jex.me>
2023-03-01 14:14:50 +0800

    Fix `queryESMBuild` function

--- server/esm.go
+++ server/esm.go
@@ -223,6 +223,9 @@ func queryESMBuild(id string) (*ESM, bool) {
 		var esm ESM
 		err = json.Unmarshal(value, &esm)
 		if err == nil {
+			if strings.HasPrefix(id, "stable/") {
+				id = fmt.Sprintf("v%d/", STABLE_VERSION) + strings.TrimPrefix(id, "stable/")
+			}
 			_, err = fs.Stat(path.Join("builds", id))
 			if err == nil {
 				return &esm, true
