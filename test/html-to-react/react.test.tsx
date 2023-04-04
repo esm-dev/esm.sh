b6f0b2434b670ce12c135cb99c3b5056bb0ff373
X <i@jex.me>
2023-04-04 21:56:55 +0800

    Add html-to-react test

---
+++ test/html-to-react/react.test.tsx
@@ -0,0 +1,14 @@
+import { assert } from "https://deno.land/std@0.178.0/testing/asserts.ts";
+
+import React from "http://localhost:8080/react@18?dev";
+import { renderToString } from "http://localhost:8080/react-dom@18/server";
+import { Parser } from "http://localhost:8080/html-to-react?dev&deps=react@18";
+
+Deno.test("html-to-react", async () => {
+  const h = new Parser();
+  const App = () => {
+    return h.parse(`<h1>Hi :)</h1>`);
+  };
+  const html = renderToString(<App />);
+  assert(typeof html === "string" && html.includes("<h1>Hi :)</h1>"));
+});
