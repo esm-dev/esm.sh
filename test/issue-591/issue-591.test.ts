aacd8f1fb9071925a3bb5ced4792692b35fbfacd
X <i@jex.me>
2023-04-17 21:53:57 +0800

    Add testing for #591

---
+++ test/issue-591/issue-591.test.ts
@@ -0,0 +1,7 @@
+import { assert } from "https://deno.land/std@0.178.0/testing/asserts.ts";
+
+import urlRegexSafe from "http://localhost:8080/url-regex-safe@3.0.0";
+
+Deno.test("issue #591", async () => {
+  assert(urlRegexSafe() instanceof RegExp);
+});
