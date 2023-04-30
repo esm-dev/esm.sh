f6ddc48cc4a81dff6bfab7b407951c8084ab8715
X <i@jex.me>
2023-04-30 11:20:39 +0800

    Update testings

--- test/issue-580/issue-580.test.ts
+++ test/issue-580/issue-580.test.ts
@@ -4,9 +4,12 @@ import {
 } from "https://deno.land/std@0.180.0/testing/asserts.ts";
 
 Deno.test("issue #580", async () => {
-  let res = await fetch("http://localhost:8080/v113/pocketbase@0.13.1");
+  const { version } = await fetch("http://localhost:8080/status.json").then((
+    res,
+  ) => res.json());
+  let res = await fetch(`http://localhost:8080/v${version}/pocketbase@0.13.1`);
   const dtsUrl =
-    "http://localhost:8080/v113/pocketbase@0.13.1/dist/pocketbase.es.d.mts";
+    `http://localhost:8080/v${version}/pocketbase@0.13.1/dist/pocketbase.es.d.mts`;
   const dtsHeader = res.headers.get("x-typescript-types");
   res.body?.cancel();
   assertEquals(dtsHeader, dtsUrl);
--- test/issue-611/issue-611.test.ts
+++ test/issue-611/issue-611.test.ts
@@ -2,6 +2,6 @@ import { assertEquals } from "https://deno.land/std@0.180.0/testing/asserts.ts";
 
 import { exists } from "http://localhost:8080/@kwsites/file-exists@1.1.1";
 
-Deno.test("issue #611", async () => {
+Deno.test("issue #611", async () => {14
   assertEquals(typeof exists, "function");
 });
