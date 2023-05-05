307a8a64ee8e92d62c7e0c370bed1215b88a3bfd
X <i@jex.me>
2023-05-05 18:30:45 +0800

    Upgrade `@types/react@18` to 18.2.5

--- server/consts.go
+++ server/consts.go
@@ -18,7 +18,7 @@ const (
 // fix some package versions
 var fixedPkgVersions = map[string]string{
 	"@types/react@17": "17.0.58",
-	"@types/react@18": "18.2.0",
+	"@types/react@18": "18.2.5",
 	"isomorphic-ws@4": "5.0.0",
 	"resolve@1.22":    "1.22.2", // 1.22.3+ will read package.json from disk
 }
