6885cb47c27be07425a9533bf13f2fef33c0bcb6
X <i@jex.me>
2023-05-06 14:33:04 +0800

    Add testing for types only package

---
+++ test/types-only/types-only.test.ts
@@ -0,0 +1,21 @@
+import { assertEquals,assertStringIncludes } from "https://deno.land/std@0.180.0/testing/asserts.ts";
+
+Deno.test("types only", async () => {
+  const { version } = await fetch("http://localhost:8080/status.json").then(
+    (r) => r.json(),
+  );
+  const res = await fetch(
+    "http://localhost:8080/@octokit-next/types-rest-api@2.5.0",
+  );
+  res.body?.cancel();
+  assertEquals(res.status, 200);
+  assertEquals(
+    res.headers.get("content-type"),
+    "application/javascript; charset=utf-8",
+  );
+  const dtsUrl =
+    `http://localhost:8080/v${version}/@octokit-next/types-rest-api@2.5.0/index.d.ts`;
+  assertEquals(res.headers.get("x-typescript-types"), dtsUrl);
+  const dts = await fetch(dtsUrl).then((r) => r.text());
+  assertStringIncludes(dts, `declare module "http://localhost:8080/v${version}/@octokit-next/types@2.5.0/index.d.ts"`)
+});
