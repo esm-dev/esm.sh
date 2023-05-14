300daebc1b400c68432c435611b34b40a03f3fbc
X <i@jex.me>
2023-05-14 14:10:02 +0800

    Add testing for `webtorrent`

---
+++ test/webtorrent/webtorrent.test.ts
@@ -0,0 +1,7 @@
+import { assertEquals } from "https://deno.land/std@0.180.0/testing/asserts.ts";
+
+import webtorrent from "http://localhost:8080/webtorrent@2.0.18?target=es2022";
+
+Deno.test("webtorrent", async () => {
+  assertEquals(typeof webtorrent, "function");
+});
