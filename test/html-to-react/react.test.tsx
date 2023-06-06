58553cac6b755266c7ae7c912603234c581ceb47
X <i@jex.me>
2023-06-06 21:22:26 +0800

    Fix html-to-react testing

--- test/html-to-react/react.test.tsx
+++ test/html-to-react/react.test.tsx
@@ -1,10 +1,10 @@
 import { assert } from "https://deno.land/std@0.180.0/testing/asserts.ts";
 
-import React from "http://localhost:8080/react@18?dev";
-import { renderToString } from "http://localhost:8080/react-dom@18/server";
-import { Parser } from "http://localhost:8080/html-to-react?dev&deps=react@18";
+import React from "http://localhost:8080/react@18.2.0";
+import { renderToString } from "http://localhost:8080/react-dom@18.2.0/server";
+import { Parser } from "http://localhost:8080/html-to-react@1.5.0?deps=react@18.2.0";
 
-Deno.test("html-to-react", async () => {
+Deno.test("html-to-react", () => {
   const h = new Parser();
   const App = () => {
     return h.parse(`<h1>Hi :)</h1>`);
