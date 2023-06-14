747cb7ad6bd85e233be4b897c6ebc7e9b58e4e0a
X <i@jex.me>
2023-06-14 14:32:40 +0800

    Fix outdated testing

--- test/issue-512/issue-512.test.ts
+++ test/issue-512/issue-512.test.ts
@@ -1,9 +1,9 @@
-import { assertEquals } from "https://deno.land/std@0.180.0/testing/asserts.ts";
+import { assertArrayIncludes } from "https://deno.land/std@0.180.0/testing/asserts.ts";
 
-import * as mod from "http://localhost:8080/react-svg-spinners@0.3.1?cjs-exports=NinetyRing,NinetyRingWithBg";
+import * as mod from "http://localhost:8080/react-svg-spinners@0.3.1";
 
 Deno.test("issue #512", () => {
-  assertEquals(Object.keys(mod).sort(), [
+  assertArrayIncludes(Object.keys(mod), [
     "NinetyRing",
     "NinetyRingWithBg",
     "default",
