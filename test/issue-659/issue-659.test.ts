eb7f89214ebfb2ff455eb3a558c560ef25487550
X <i@jex.me>
2023-06-15 20:40:28 +0800

    Add testing for #659

---
+++ test/issue-659/issue-659.test.ts
@@ -0,0 +1,7 @@
+import { assertEquals } from "https://deno.land/std@0.180.0/testing/asserts.ts";
+
+import { createNodeMiddleware } from "http://localhost:8080/@octokit/oauth-app@4.2.2";
+
+Deno.test("issue #659", () => {
+  assertEquals(typeof createNodeMiddleware, "function");
+});
