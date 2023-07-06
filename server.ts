200f49f38beb3a765693ff533907cd96e667fd69
X <i@jex.me>
2023-07-06 19:48:11 +0800

    Bump esm-worker to 0.128.1

--- server.ts
+++ server.ts
@@ -11,8 +11,8 @@ import type {
   Context,
   HttpMetadata,
   WorkerStorage,
-} from "https://esm.sh/esm-worker@0.128.0";
-import { withESMWorker } from "https://esm.sh/esm-worker@0.128.0";
+} from "https://esm.sh/esm-worker@0.128.1";
+import { withESMWorker } from "https://esm.sh/esm-worker@0.128.1";
 
 type Handler = (
   request: Request,
