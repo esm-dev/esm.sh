0d43246659f734dc571ade51c3d2c3b6a96dbf3a
X <i@jex.me>
2023-11-15 22:49:27 +0800

    Add testing for issue #743

---
+++ test/issue-743/issue-743.test.ts
@@ -0,0 +1,7 @@
+import { assertEquals } from "https://deno.land/std@0.180.0/testing/asserts.ts";
+
+import OpenAI from "http://localhost:8080/openai@4.12.4";
+
+Deno.test("issue #743", () => {
+  assertEquals(typeof OpenAI, "function");
+});
