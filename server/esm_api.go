7e738d92adc26eee0e818bceb5c75858d737af17
X <i@jex.me>
2023-11-29 01:36:28 +0800

    Set input code size limitation

--- server/esm_api.go
+++ server/esm_api.go
@@ -35,13 +35,16 @@ func apiHandler() rex.Handle {
 			switch ctx.Path.String() {
 			case "/transform", "/build":
 				var input BuildInput
-				err := json.NewDecoder(io.LimitReader(ctx.R.Body, 1024*1024)).Decode(&input)
+				err := json.NewDecoder(io.LimitReader(ctx.R.Body, 512*1024)).Decode(&input)
 				ctx.R.Body.Close()
 				if err != nil {
-					return rex.Err(400, "invalid body content type")
+					return rex.Err(400, "require valid json body")
 				}
 				if input.Code == "" {
 					return rex.Err(400, "code is required")
+				}
+				if len(input.Code) > 100*1024 {
+					return rex.Err(429, "code is too large")
 				}
 				if !input.TransformOnly {
 					input.TransformOnly = ctx.Path.String() == "/transform"
