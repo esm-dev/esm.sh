d052a913d15c186312c411c1f19e6067947ae6b2
X <i@jex.me>
2023-12-05 15:19:59 +0800

    Recover test app

--- packages/esm-hot/test/App.tsx
+++ packages/esm-hot/test/App.tsx
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 export default function App() {
   return <h1>Hello World!</h1>;
 =======
@@ -9,4 +10,8 @@ export default function App() {
   useState(0);
   return <h1 style={{ fontSize: 12 }}>Hello World!</h1>;
 >>>>>>> 67943aa (Add `addModule`command)
+=======
+export default function App() {
+  return <h1>Hello World!</h1>;
+>>>>>>> f58f7dd (Recover test app)
 }
