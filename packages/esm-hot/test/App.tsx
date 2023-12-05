762ee7974869c0c5ad7dd766c9388189b0b9a466
X <i@jex.me>
2023-12-05 15:53:19 +0800

    Fix conflict shit

--- packages/esm-hot/test/App.tsx
+++ packages/esm-hot/test/App.tsx
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 export default function App() {
   return <h1>Hello World!</h1>;
-=======
-import { useState } from "react";
-import { useEffect } from "preact/hooks";
-
-export default function App() {
-  useState(0);
-  return <h1 style={{ fontSize: 12 }}>Hello World!</h1>;
->>>>>>> 67943aa (Add `addModule`command)
-=======
-export default function App() {
-  return <h1>Hello World!</h1>;
->>>>>>> f58f7dd (Recover test app)
 }
