48da57b51c6b6a949b53519e99f05a9db057362d
X <i@jex.me>
2023-12-05 14:25:57 +0800

    Add `addModule`command

--- packages/esm-hot/test/App.tsx
+++ packages/esm-hot/test/App.tsx
@@ -1,3 +1,12 @@
+<<<<<<< HEAD
 export default function App() {
   return <h1>Hello World!</h1>;
+=======
+import { useState } from "react";
+import { useEffect } from "preact/hooks";
+
+export default function App() {
+  useState(0);
+  return <h1 style={{ fontSize: 12 }}>Hello World!</h1>;
+>>>>>>> 67943aa (Add `addModule`command)
 }
