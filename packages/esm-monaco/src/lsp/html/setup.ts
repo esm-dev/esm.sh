20e24ac5511d2e9c69b3b198c556c24dbc635064
X <i@jex.me>
2024-01-27 22:08:47 +0800

    esm-monaco: implement html lsp worker

--- packages/esm-monaco/src/lsp/html/setup.ts
+++ packages/esm-monaco/src/lsp/html/setup.ts
@@ -1,5 +1,92 @@
 import * as monacoNS from "monaco-editor-core";
+import * as lsTypes from "../ls-types";
+import type { CreateData, HTMLWorker } from "./worker";
 
-export function setup(id: string, monaco: typeof monacoNS) {
-  console.log("setup", id, monaco);
+class HTMLCompletionAdapter extends lsTypes.CompletionAdapter<HTMLWorker> {
+  constructor(worker: lsTypes.WorkerAccessor<HTMLWorker>) {
+    super(worker, [".", ":", "<", '"', "=", "/"]);
+  }
+}
+
+export function setup(languageId: string, monaco: typeof monacoNS) {
+  const languages = monaco.languages;
+  const createData: CreateData = {
+    languageId,
+    options: {
+      data: {
+        useDefaultDataProvider: true,
+      },
+      suggest: {},
+      format: {
+        tabSize: 4,
+        insertSpaces: false,
+        wrapLineLength: 120,
+        unformatted:
+          'default": "a, abbr, acronym, b, bdo, big, br, button, cite, code, dfn, em, i, img, input, kbd, label, map, object, q, samp, select, small, span, strong, sub, sup, textarea, tt, var',
+        contentUnformatted: "pre",
+        indentInnerHtml: false,
+        preserveNewLines: true,
+        maxPreserveNewLines: undefined,
+        indentHandlebars: false,
+        endWithNewline: false,
+        extraLiners: "head, body, /html",
+        wrapAttributes: "auto",
+      },
+    },
+  };
+  const worker = monaco.editor.createWebWorker<HTMLWorker>({
+    moduleId: "lsp/css/worker",
+    label: languageId,
+    createData,
+  });
+  const workerAccessor: lsTypes.WorkerAccessor<HTMLWorker> = (
+    ...uris: monacoNS.Uri[]
+  ): Promise<HTMLWorker> => {
+    return worker.withSyncedResources(uris);
+  };
+
+  lsTypes.preclude(monaco);
+  languages.registerCompletionItemProvider(
+    languageId,
+    new HTMLCompletionAdapter(workerAccessor),
+  );
+  languages.registerHoverProvider(
+    languageId,
+    new lsTypes.HoverAdapter(workerAccessor),
+  );
+  languages.registerDocumentHighlightProvider(
+    languageId,
+    new lsTypes.DocumentHighlightAdapter(workerAccessor),
+  );
+  languages.registerLinkProvider(
+    languageId,
+    new lsTypes.DocumentLinkAdapter(workerAccessor),
+  );
+  languages.registerFoldingRangeProvider(
+    languageId,
+    new lsTypes.FoldingRangeAdapter(workerAccessor),
+  );
+  languages.registerDocumentSymbolProvider(
+    languageId,
+    new lsTypes.DocumentSymbolAdapter(workerAccessor),
+  );
+  languages.registerSelectionRangeProvider(
+    languageId,
+    new lsTypes.SelectionRangeAdapter(workerAccessor),
+  );
+  languages.registerRenameProvider(
+    languageId,
+    new lsTypes.RenameAdapter(workerAccessor),
+  );
+  // only html
+  if (languageId === "html") {
+    languages.registerDocumentFormattingEditProvider(
+      languageId,
+      new lsTypes.DocumentFormattingEditProvider(workerAccessor),
+    );
+    languages.registerDocumentRangeFormattingEditProvider(
+      languageId,
+      new lsTypes.DocumentRangeFormattingEditProvider(workerAccessor),
+    );
+  }
 }
