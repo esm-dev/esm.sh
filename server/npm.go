88bf659a8273f414ef48c20c961907da0570a378
X <i@jex.me>
2024-03-03 11:22:52 +0800

    Improve `installPackage` function

--- server/npm.go
+++ server/npm.go
@@ -352,17 +352,17 @@ func fetchPackageInfo(name string, version string) (info NpmPackageInfo, err err
 }
 
 func installPackage(wd string, pkg Pkg) (err error) {
+	pkgVersionName := pkg.VersionName()
+	lock := getInstallLock(pkgVersionName)
+
+	// only one install process allowed at the same time
+	lock.Lock()
+	defer lock.Unlock()
+
 	// skip install if pnpm lock file exists
 	if fileExists(path.Join(wd, "pnpm-lock.yaml")) && fileExists(path.Join(wd, "node_modules", pkg.Name, "package.json")) {
 		return nil
 	}
-
-	pkgVersionName := pkg.VersionName()
-
-	// only one install process allowed at the same time
-	lock := getInstallLock(pkgVersionName)
-	lock.Lock()
-	defer lock.Unlock()
 
 	// ensure package.json file to prevent read up-levels
 	packageFilePath := path.Join(wd, "package.json")
