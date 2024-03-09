24becff17ebe7ab563c37ee2dd71882353cb448a
X <i@jex.me>
2024-03-09 21:54:17 +0800

    Remove testing page

--- server/embed/index.js
+++ server/embed/index.js
@@ -7,8 +7,10 @@ import bash from "{origin}{basePath}/highlight.js@11.9.0/lib/languages/bash";
 
 export function render(md) {
   const mainEl = document.querySelector("main");
-  mainEl.innerHTML = marked.parse(md.split("# esm.sh")[1]) +
-    `<p class="link"><a href="./?test">Testing &rarr; </a></p>`;
+  mainEl.innerHTML = marked.parse(md.split("# esm.sh")[1]);
+  mainEl.querySelectorAll("h1,h2,h3").forEach((el) => {
+    el.id = el.textContent.toLowerCase().replace(/[^a-z0-9]+/g, "-");
+  });
   mainEl.querySelectorAll("code.language-bash").forEach((block) => {
     block.innerHTML = block.innerHTML.replace(/(^|\n)\$ /g, "$1");
   });
@@ -16,7 +18,7 @@ export function render(md) {
   // remove badges
   document.querySelectorAll("a > img[src^='https://img.shields.io/']").forEach((img) => {
     img.parentElement.remove();
-  })
+  });
 
   // scroll to hashHeading
   const hashHeading = document.getElementById(location.hash.slice(1));
