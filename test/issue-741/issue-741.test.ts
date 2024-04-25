6e329447ad41dc34c615146b8fae898fe12befaa
X <i@jex.me>
2024-04-25 21:02:53 +0800

    Add testing for issue-741

---
+++ test/issue-741/issue-741.test.ts
@@ -0,0 +1,13 @@
+// close https://github.com/esm-dev/esm.sh/issues/741
+
+import { assertEquals } from "https://deno.land/std@0.220.0/assert/mod.ts";
+
+import { Kind, type TSchema, TypeRegistry } from "http://localhost:8080/@sinclair/typebox@0.32.22?no-bundle";
+import { Value } from "http://localhost:8080/@sinclair/typebox@0.32.22/value?no-bundle";
+
+Deno.test("issue #741", () => {
+  const Foo = { [Kind]: "Foo" } as TSchema;
+  TypeRegistry.Set("Foo", (_, value) => value === "foo");
+  assertEquals(Value.Check(Foo, "foo"), true);
+  assertEquals(Value.Check(Foo, "bar"), false);
+});
