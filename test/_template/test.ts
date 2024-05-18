48d3c06d58d40149109d37f59b3a400b3a1e7d0b
X <i@jex.me>
2024-05-18 23:51:29 +0800

    Add integration testing template

---
+++ test/_template/test.ts
@@ -0,0 +1,11 @@
+import { assert, assertEquals, assertStringIncludes } from "assert";
+
+// change the import path to the module you want to test
+import * as mod from "~/PKG[@SEMVER][/PATH]";
+
+// change the test name and the test assertions
+Deno.test("foo", () => {
+  assert("foo" in mod);
+  assertEquals(typeof mod.foo(), "function");
+  assertStringIncludes(mod.foo(), "bar");
+});
