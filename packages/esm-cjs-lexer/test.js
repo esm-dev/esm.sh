d2c3efb5ee5431d0f8693bc521cdeb2cbeeead8e
X <i@jex.me>
2024-06-12 16:44:51 +0800

    esm-cjs-lexer: Update deps

--- packages/esm-cjs-lexer/test.js
+++ packages/esm-cjs-lexer/test.js
@@ -11,9 +11,15 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 const code = fs.readFileSync("./test.js", "utf-8")
-const ret = parse("./test.js", code, { nodeEnv: "development" })
 
+const ret = parse("./test.js", code, { nodeEnv: "development" })
 if (ret.exports.join(',') !== 'foo,bar,dev') {
   throw new Error('unexpected exports of index.js: ' + ret.exports.join(','))
 }
+
+const ret2 = parse("./test.js", code, { nodeEnv: "production" })
+if (ret2.exports.join(',') !== 'foo,bar,prod') {
+  throw new Error('unexpected exports of index.js: ' + ret2.exports.join(','))
+}
+
 console.log("done")
