476dfc4b7dbe425abe08de921c9739eaf7033cb8
X <i@jex.me>
2024-06-15 15:08:07 +0800

    test: Update testings

--- test/baseui/test.ts
+++
@@ -1,7 +0,0 @@
-import { assertEquals } from "https://deno.land/std@0.220.0/assert/mod.ts";
-
-import { createTheme } from "http://localhost:8080/baseui@12.2.0";
-
-Deno.test("baseui", () => {
-  assertEquals(typeof createTheme, "function");
-});
