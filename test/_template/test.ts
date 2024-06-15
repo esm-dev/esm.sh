4e386113862f5313eba8c65fcde1847b42391580
X <i@jex.me>
2024-06-15 23:47:13 +0800

    test: Update testing template

--- test/_template/test.ts
+++ test/_template/test.ts
@@ -1,10 +1,11 @@
-import { assert, assertEquals, assertStringIncludes } from "assert";
+// related issue: https://github.com/esm-dev/esm.sh/issues/[ISSUE_NUMBER]
 
 // change the import path to the module you want to test
 import * as mod from "~/PKG[@SEMVER][/PATH]";
+import { assert, assertEquals, assertStringIncludes } from "assert";
 
 // change the test name and the test assertions
-Deno.test("foo", () => {
+Deno.test("TEST NAME", async () => {
   assert("foo" in mod);
   assertEquals(typeof mod.foo, "function");
   assertStringIncludes(mod.foo(), "bar");
