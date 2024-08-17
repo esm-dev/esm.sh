d3aef9896d7d93f6dc7f0b95bf56767051fa6288
X <i@jex.me>
2024-08-17 20:07:54 +0800

    Move testings

--- test/common/typescript.test.ts
+++ test/common/typescript.test.ts
@@ -1,12 +1,21 @@
 import { assert, assertEquals, assertStringIncludes } from "jsr:@std/assert";
 
-import * as ts from "http://localhost:8080/typescript@5.5.4?target=esnext";
+import * as ts from "http://localhost:8080/typescript@5.5.4";
+import * as ts2 from "http://localhost:8080/typescript@5.5.4?target=esnext";
 
-Deno.test("typescript (target=esnext)", async () => {
+Deno.test("typescript", () => {
   const result = ts.transpileModule(`let x: string  = "string"`, {
     compilerOptions: { module: ts.ModuleKind.CommonJS },
   });
   assertEquals(ts.version, "5.5.4");
+  assertEquals(result.outputText, `var x = "string";\n`);
+});
+
+Deno.test("typescript (target=esnext)", async () => {
+  const result = ts2.transpileModule(`let x: string  = "string"`, {
+    compilerOptions: { module: ts2.ModuleKind.CommonJS },
+  });
+  assertEquals(ts2.version, "5.5.4");
   assertEquals(result.outputText, `var x = "string";\n`);
 
   const res = await fetch("http://localhost:8080/typescript@5.5.4/esnext/typescript.mjs");
--- test/jsx-runtime/test.tsx
+++ test/jsx-runtime/test.tsx
@@ -1,9 +1,27 @@
-import { assertStringIncludes } from "jsr:@std/assert";
+import { assert, assertStringIncludes } from "jsr:@std/assert";
 
 import { Airplay } from "http://localhost:8080/gh/phosphor-icons/react@v2.1.5/src/csr/Airplay.tsx?deps=react@18.2.0";
 import { renderToString } from "http://localhost:8080/react-dom@18.2.0/server";
 
 Deno.test("jsx-runtime", async () => {
+  const html = renderToString(
+    <main>
+      <h1>Hi :)</h1>
+    </main>,
+  );
+  assert(
+    typeof html === "string" &&
+      html.includes("<h1>Hi :)</h1>") &&
+      html.includes("<main") && html.includes("</main>"),
+  );
+});
+
+Deno.test("react-jsx-runtime deps", async () => {
+  const res = await fetch("http://localhost:8080/react@18.2.0/esnext/jsx-runtime.js");
+  assertStringIncludes(await res.text(), `from "/react@18.2.0/esnext/react.mjs"`);
+});
+
+Deno.test("rendering a svg from github.com", async () => {
   const svg = renderToString(<Airplay />);
   assertStringIncludes(svg, "<svg ");
   assertStringIncludes(svg, "</svg>");
