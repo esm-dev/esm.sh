4291f7f793493757b640b766b2f9926bba69988c
X <i@jex.me>
2024-08-18 16:17:35 +0800

    Add `compat` testing

--- test/compat/test.ts
+++ test/compat/test.ts
@@ -1,11 +1,11 @@
 import { assertStringIncludes } from "jsr:@std/assert";
 
-Deno.test("issue #509", async () => {
+Deno.test("HeadlessChrome", async () => {
   const res = await fetch("http://localhost:8080/react@18.2.0", {
     headers: {
       "User-Agent": "HeadlessChrome/109",
     },
   });
   const text = await res.text();
-  assertStringIncludes(text, "/es2023/");
+  assertStringIncludes(text, "/es2021/");
 });
