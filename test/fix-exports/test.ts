a409c10ed9fcc1ebd5e59472d4c6c6c4eb989027
Je Xia <i@jex.me>
2024-10-15 03:47:57 +0800

    Update testings

--- test/fix-exports/test.ts
+++ test/fix-exports/test.ts
@@ -3,8 +3,6 @@ import { assertEquals } from "jsr:@std/assert";
 import { BigInteger } from "http://localhost:8080/jsbn@1.1.0";
 import { Netmask } from "http://localhost:8080/netmask@2.0.2";
 import { parseStringPromise } from "http://localhost:8080/xml2js@0.4.23";
-import hljs from "http://localhost:8080/highlight.js@11.9.0/lib/core";
-import type { HLJSApi } from "http://localhost:8080/highlight.js@11.9.0";
 import compareVersions from "http://localhost:8080/tiny-version-compare@3.0.1";
 import { createTheme } from "http://localhost:8080/baseui@12.2.0";
 
@@ -12,8 +10,6 @@ Deno.test("fix some invalid exports", () => {
   assertEquals(typeof BigInteger, "function");
   assertEquals(typeof Netmask, "function");
   assertEquals(typeof parseStringPromise, "function");
-  assertEquals(typeof (hljs satisfies HLJSApi), "object");
-  assertEquals(typeof hljs.highlight, "function");
   assertEquals(typeof createTheme, "function");
   assertEquals(typeof compareVersions, "function");
 });
