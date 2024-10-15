879021baca38b96120559d986b3b1e7af6795fb7
Je Xia <i@jex.me>
2024-10-16 01:58:26 +0800

    Move testings

--- test/_issue-888/test.ts
+++
@@ -1,8 +0,0 @@
-import { assertEquals } from "jsr:@std/assert";
-
-import * as hljs from "http://localhost:8080/highlight.js@11.9.0";
-
-// https://github.com/esm-dev/esm.sh/issues/888
-Deno.test("issue-888", () => {
-  console.log(hljs);
-});
