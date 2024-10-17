91754b20e66d391dfae5f18f1a13258de84659e8
Je Xia <i@jex.me>
2024-10-17 10:24:57 +0800

    Update run script

--- server/embed/run.ts
+++ server/embed/run.ts
@@ -10,35 +10,26 @@
     const { hostname, href, pathname, search } = location;
     if (src === modUrl && main) {
       if (hostname === "localhost" || hostname === "127.0.0.1" || /^192\.168\.\d+\.\d+$/.test(hostname)) {
-        fetch(main).then((res) => {
-          if (res.ok) {
-            if (res.headers.get("server") === "esm.sh/run") {
-              import(main);
-            } else {
-              alert("Please serve your app with `npx esm.sh run` for local development.");
-            }
-          }
-        });
-      } else {
-        const mainUrl = new URL(main, href);
-        const q = mainUrl.searchParams;
-        const v = q.get("v") || q.get("version");
-        mainUrl.search = "";
-        if ($("script[type=importmap]")) {
-          q.set(
-            "im",
-            (HTMLScriptElement.supports("importmap") ? "y" : "N") +
-              btoa(pathname + search).replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, ""),
-          );
-        }
-        if (v) {
-          q.set("v", v);
-        }
-        import(new URL(src).origin + "/" + mainUrl);
+        alert("Please serve your app with `npx esm.sh run` for local development.");
+        return;
       }
+      const mainUrl = new URL(main, href);
+      const q = mainUrl.searchParams;
+      const v = q.get("v") || q.get("version");
+      mainUrl.search = "";
+      if ($("script[type=importmap]")) {
+        q.set(
+          "im",
+          (HTMLScriptElement.supports("importmap") ? "y" : "N")
+            + btoa(pathname).replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, ""),
+        );
+      }
+      if (v) {
+        q.set("v", v);
+      }
+      import(new URL(src).origin + "/" + mainUrl);
     }
   }
-
   // compatibility with esm.sh/run(v1) which has been renamed to 'esm.sh/tsx'
   if ($<HTMLScriptElement>("script[type^='text/']")) {
     import(new URL("/tsx", modUrl).href);
