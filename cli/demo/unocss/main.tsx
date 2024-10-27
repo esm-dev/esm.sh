fa173b8eb3327e7edbaab4fbd2fe19b3e6970af7
Je Xia <i@jex.me>
2024-10-27 23:02:20 +0800

    cli: update demo apps

--- cli/demo/unocss/main.tsx
+++ cli/demo/unocss/main.tsx
@@ -2,6 +2,6 @@ import { render } from "preact";
 import { App } from "./App.tsx";
 
 const root = document.getElementById("root")!;
-root.replaceChildren();
+root.replaceChildren(); // remove loading placeholder
 
 render(<App />, root);
