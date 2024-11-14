35d6476868d8839427b2ae85a6a0388393ef372e
Je Xia <i@jex.me>
2024-11-14 21:06:57 +0800

    Fix `is-number` polyfill

--- server/embed/polyfills/npm/is-number.mjs
+++ server/embed/polyfills/npm/is-number.mjs
@@ -1,1 +1,2 @@
-export default v=>typeof v==="number";
+// from @talentlessguy
+export default v => (typeof v === "number" && v - v === 0) || (typeof v === "string" && Number.isFinite(+v) && v.trim() !== "");
