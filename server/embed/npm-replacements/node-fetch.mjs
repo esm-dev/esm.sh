0ff287336433d1596566393d5fca5d7d5d636b6c
Je Xia <i@jex.me>
2024-11-19 17:12:10 +0800

    simplify node-fetch.mjs

--- server/embed/npm-replacements/node-fetch.mjs
+++ server/embed/npm-replacements/node-fetch.mjs
@@ -1,18 +1,13 @@
-export const Blob = globalThis.Blob;
-export const File = globalThis.File;
-export const FormData = globalThis.FormData;
-export const Headers = globalThis.Headers;
-export const Request = globalThis.Request;
-export const Response = globalThis.Response;
-export const AbortController = globalThis.AbortController;
-
-export const fetch = globalThis.fetch ?? (() => {
-  throw new Error("global fetch is not available!");
-});
+export const { Blob, fetch, File, FormData, Headers, Request, Response, AbortController } = globalThis;
+fetch.Promise = globalThis.Promise;
+fetch.isRedirect = isRedirect;
 export default fetch;
 
-export class AbortError extends Error {}
-export class FetchError extends Error {}
+export const AbortError = Error;
+export const FetchError = Error;
+
+const redirectStatus = /* @__PURE__ */ new Set([301, 302, 303, 307, 308]);
+export const isRedirect = (code) => redirectStatus.has(code);
 
 export async function blobFrom(path, type) {
   if (typeof Deno !== "undefined") {
@@ -47,15 +42,3 @@ export function fileFromSync(path, type) {
   }
   throw new Error("blobFromSync is not supported in browser");
 }
-
-const redirectStatus = new Set([301, 302, 303, 307, 308]);
-
-/**
- * Redirect code matching
- *
- * @param {number} code - Status code
- * @return {boolean}
- */
-export const isRedirect = (code) => {
-  return redirectStatus.has(code);
-};
