64e3de584439ccf7a6a7ffc65537077ee12087af
Je Xia <i@jex.me>
2024-12-25 21:10:44 +0800

    fix unocss loader compiling

--- server/loader_implements.go
+++ server/loader_implements.go
@@ -197,7 +197,6 @@ func compileUnocssLoader(npmrc *NpmRC, loaderVersion string, loaderExecPath stri
 		"deno", "compile",
 		"--no-config",
 		"--no-lock",
-		"--cached-only",
 		"--no-check",
 		"--include=jsr:@std/tar/untar-stream",
 		"--no-prompt",
