44a0c647e30c13a2ab0770849e61aabc71217d38
Je Xia <i@jex.me>
2025-02-22 20:09:47 +0800

    Update testing

--- test/gh/jsr.test.ts
+++ test/gh/jsr.test.ts
@@ -4,4 +4,5 @@ import $ from "http://localhost:8080/gh/dsherret/dax";
 
 Deno.test("jsr package from github", async () => {
   assertEquals(typeof $, "function");
+  assertEquals(await $`echo 42`.text(), "42");
 });
