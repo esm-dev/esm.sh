bcbc02373d7fb3914603d9deea274592647fadc2
Je Xia <i@jex.me>
2025-03-23 15:21:17 +0800

    refactor

--- server/build.go
+++ server/build.go
@@ -145,7 +145,7 @@ func (ctx *BuildContext) Build() (meta *BuildMeta, err error) {
 	}
 
 	// analyze splitting modules
-	if !ctx.pkgJson.SideEffectsFalse {
+	if !ctx.pkgJson.SideEffectsFalse && ctx.bundleMode == BundleDefault && ctx.pkgJson.Exports.Len() > 1 {
 		ctx.status = "analyze"
 		err = ctx.analyzeSplitting()
 		if err != nil {
