a4ab4bc8e7798aad620d09cedc6cff41f24c911f
Je Xia <i@jex.me>
2025-04-28 21:45:54 +0800

    test: Fix version of /gh packages

--- test/gh/fluentui-emoji.test.ts
+++ test/gh/fluentui-emoji.test.ts
@@ -2,7 +2,7 @@ import { assertEquals, assertStringIncludes } from "jsr:@std/assert";
 
 Deno.test("github assets", async () => {
   const res = await fetch(
-    "http://localhost:8080/gh/microsoft/fluentui-emoji/assets/Alien/Flat/alien_flat.svg",
+    "http://localhost:8080/gh/microsoft/fluentui-emoji@62ecdc0/assets/Alien/Flat/alien_flat.svg",
   );
   assertEquals(res.status, 200);
   assertEquals(res.headers.get("content-type"), "image/svg+xml; charset=utf-8");
