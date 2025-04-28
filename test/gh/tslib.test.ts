8b0cd0de96dcdbb54fbd651216ebf6b8161c9496
Je Xia <i@jex.me>
2025-04-28 22:21:48 +0800

    Fix legacy dts hostname (#1129)

--- test/gh/tslib.test.ts
+++ test/gh/tslib.test.ts
@@ -1,6 +1,6 @@
 import { assertExists } from "jsr:@std/assert";
 
-import * as tslib from "http://localhost:8080/gh/microsoft/tslib";
+import * as tslib from "http://localhost:8080/gh/microsoft/tslib@v2.8.1";
 
 Deno.test("tslib from github", async () => {
   assertExists(tslib.__await);
