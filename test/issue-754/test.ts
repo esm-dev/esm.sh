936aaa5264244b8d720e4b8107f96a5d095c418e
X <i@jex.me>
2025-07-02 20:15:56 +0800

    Update testings

--- test/issue-754/test.ts
+++ test/issue-754/test.ts
@@ -7,10 +7,12 @@ import proj4 from "http://localhost:8080/proj4@2.9.2";
 
 Deno.test("issue #754", () => {
   const rdId = "EPSG:28992";
+  // @ts-ignore
   proj4.defs(
     rdId,
     "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.417,50.3319,465.552,-0.398957,0.343988,-1.8774,4.0725 +units=m +no_defs",
   );
+  // @ts-ignore
   register(proj4);
   const rdProjection = new Projection({
     code: rdId,
