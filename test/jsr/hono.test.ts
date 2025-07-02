505f7aba37305673b5677cb77cb3862ebbb5e1d7
X <i@jex.me>
2025-07-02 22:35:05 +0800

    Update testings

--- test/jsr/hono.test.ts
+++ test/jsr/hono.test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "jsr:@std/assert";
 
-import { Hono } from "https://esm.sh/jsr/@hono/hono@4";
+import { Hono } from "http://localhost:8080/jsr/@hono/hono@4";
 
 Deno.test("hono", async () => {
   const hono = new Hono();
