776dd3903878ac0be0fc5d7da4e3776e069101e3
Je Xia <i@jex.me>
2025-09-17 09:45:36 +0800

    Update testings

--- test/issue-363/test.ts
+++ test/issue-363/test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "jsr:@std/assert";
 
-import { fs } from "http://localhost:8080/memfs";
+import { fs } from "http://localhost:8080/memfs@4.39.0";
 
 Deno.test("issue #363", () => {
   fs.writeFileSync("/hello.txt", "World!");
