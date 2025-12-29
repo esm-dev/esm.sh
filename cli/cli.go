12a3e7f9491b23796b5ad78cf9a90e7ff6cc9936
Je Xia <i@jex.me>
2025-12-29 22:19:26 +0800

    Remove update command for now

--- cli/cli.go
+++ cli/cli.go
@@ -10,8 +10,7 @@ const helpMessage = "\033[30mesm.sh - A no-build tool for modern web development
 Usage: esm.sh [command] [options]
 
 Commands:
-  add, i [...packages]    Add specified packages to the "importmap" script in index.html
-  update                  Update existing packages in the "importmap" script in index.html
+  add [...packages]    Add specified packages to the "importmap" script in index.html
   tidy                    Clean up and optimize the "importmap" script in index.html
   init                    Initialize a new web application
   serve                   Serve the web application in production mode
@@ -28,10 +27,8 @@ func Run() {
 		return
 	}
 	switch command := os.Args[1]; command {
-	case "add", "i":
+	case "add":
 		Add()
-	case "update":
-		Update()
 	case "tidy":
 		Tidy()
 	case "init":
