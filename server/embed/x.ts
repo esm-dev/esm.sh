830b687b9a59e5a49369420e7f58b4476f398c5d
Je Xia <i@jex.me>
2026-01-07 21:20:31 +0800

    Add CLI install script (#1272)

--- server/embed/x.ts
+++ server/embed/x.ts
@@ -7,17 +7,17 @@
   const currentScript = document.currentScript as HTMLScriptElement | null;
   const src = currentScript?.src;
   const href = currentScript?.getAttribute("href");
+  const meta = (name: string) => document.querySelector<HTMLMetaElement>("meta[name=" + name + "]")?.content;
   if (location.protocol === "file:" || /^(([\w\-]+\.)?local(host)?|127\.0\.0\.1)$/.test(location.hostname)) {
     console.error("[esm.sh/x] Please start your app with `esm.sh dev` in development mode.");
     return;
   }
   if (src && href) {
-    const origin = location.origin;
     const cdnOrigin = new URL(src).origin;
     const mainUrl = new URL(href, location.href);
     const searchParams = mainUrl.searchParams;
-    const version = document.querySelector<HTMLMetaElement>("meta[name=version]")?.content;
-    const basePath = document.querySelector<HTMLMetaElement>("meta[name=basepath]")?.content;
+    const version = meta("version");
+    const basePath = meta("basepath");
     if (basePath && basePath.startsWith("/")) {
       searchParams.set("b", btoa(basePath).replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/, ""));
     }
