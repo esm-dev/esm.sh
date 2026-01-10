b656b856f8d5805f20e4314bdcc4c9e878963be7
Je Xia <i@jex.me>
2026-01-10 12:48:24 +0800

    Fix testing

--- test/issue-578/test.ts
+++ test/issue-578/test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "jsr:@std/assert";
+import { assert, assertEquals } from "jsr:@std/assert";
 
 Deno.test("issue #578", async () => {
   const res = await fetch(`http://localhost:8080/katex@0.16.4/dist/katex.mjs?target=esnext`);
@@ -6,5 +6,6 @@ Deno.test("issue #578", async () => {
   const esmPathHeader = res.headers.get("X-Esm-Path");
   assertEquals(esmPathHeader, "/katex@0.16.4/esnext/katex.mjs");
   const tsHeader = res.headers.get("x-typescript-types");
-  assertEquals(tsHeader, "http://localhost:8080/@types/katex@~0.16.7/index.d.ts");
+  assert(tsHeader?.startsWith("http://localhost:8080/@types/katex@~0.16"));
+  assert(tsHeader?.endsWith("/index.d.ts"));
 });
