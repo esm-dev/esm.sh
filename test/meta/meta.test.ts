0a2d8ae36a98ec7942d54242be20867967fe9d28
Je Xia <i@jex.me>
2026-01-11 15:24:35 +0800

    Update testings

---
+++ test/meta/meta.test.ts
@@ -0,0 +1,28 @@
+import { assert, assertEquals } from "jsr:@std/assert";
+
+Deno.test("?meta query", async () => {
+  {
+    const res = await fetch("http://localhost:8080/react@19?meta", { headers: { "User-Agent": "i'm a browser" } });
+    assertEquals(res.status, 200);
+    assertEquals(res.headers.get("cache-control"), "public, max-age=600");
+    assertEquals(res.headers.get("content-type"), "application/json; charset=utf-8");
+    const meta = await res.json();
+    assertEquals(meta.name, "react");
+    assert(meta.version.startsWith("19."));
+    assert(meta.exports.includes("./jsx-runtime"));
+    assert(!meta.imports);
+  }
+  {
+    const res = await fetch("http://localhost:8080/react-dom@19.2.3?meta", { headers: { "User-Agent": "i'm a browser" } });
+    assertEquals(res.status, 200);
+    assertEquals(res.headers.get("cache-control"), "public, max-age=31536000, immutable");
+    assertEquals(res.headers.get("content-type"), "application/json; charset=utf-8");
+    const meta = await res.json();
+    assertEquals(meta.name, "react-dom");
+    assertEquals(meta.version, "19.2.3");
+
+    assert(meta.exports.includes("./client"));
+    assert(meta.exports.includes("./server"));
+    assert(meta.imports?.[0].startsWith("/react@19.2.3/"));
+  }
+});
