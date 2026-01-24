6e7ac663cbef3cdf154d3d9dbfbe3d39f0a50a9a
Je Xia <i@jex.me>
2026-01-24 19:31:25 +0800

    Improve `?meta` API (#1290)

--- test/issue-572/test.ts
+++ test/issue-572/test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "jsr:@std/assert";
 
-import { filterCookies } from "http://localhost:8080/@supabase/auth-helpers-shared@0.3.0";
+import { filterCookies } from "http://localhost:8080/@supabase/auth-helpers-shared@0.3.0?deps=@supabase/supabase-js@2.91.0";
 
 Deno.test("issue #572", async () => {
   assertEquals(filterCookies(["foo=", "bar="], "foo"), ["bar="]);
--- test/meta/meta.test.ts
+++ test/meta/meta.test.ts
@@ -23,6 +23,25 @@ Deno.test("?meta query", async () => {
 
     assert(meta.exports.includes("./client"));
     assert(meta.exports.includes("./server"));
-    assert(meta.imports?.[0].startsWith("/react@19.2.3/"));
+    assert(!meta.imports);
+    assertEquals(meta.peerImports?.length, 1);
+    assert(meta.peerImports?.[0].startsWith("/react@19.2."));
+  }
+  {
+    const res = await fetch("http://localhost:8080/react-dom@19.2.3/client?meta", { headers: { "User-Agent": "i'm a browser" } });
+    assertEquals(res.status, 200);
+    assertEquals(res.headers.get("cache-control"), "public, max-age=31536000, immutable");
+    assertEquals(res.headers.get("content-type"), "application/json; charset=utf-8");
+    const meta = await res.json();
+    assertEquals(meta.name, "react-dom");
+    assertEquals(meta.version, "19.2.3");
+    assertEquals(meta.subpath, "client");
+
+    assert(!meta.exports);
+    assertEquals(meta.imports?.length, 2);
+    assert(meta.imports?.[0].startsWith("/react-dom@19.2.3/"));
+    assert(meta.imports?.[1].startsWith("/scheduler@^0.27.0?"));
+    assertEquals(meta.peerImports?.length, 1);
+    assert(meta.peerImports?.[0].startsWith("/react@19.2."));
   }
 });
