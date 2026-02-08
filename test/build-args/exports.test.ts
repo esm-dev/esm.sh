a723a6df12bdec03db0f9ad50bb10ace469a38ea
Je Xia <i@jex.me>
2026-02-08 20:57:10 +0800

    Update testing

--- test/build-args/exports.test.ts
+++ test/build-args/exports.test.ts
@@ -7,9 +7,19 @@ Deno.test("?exports", () => {
 });
 
 Deno.test("?exports no imports", async () => {
-  const res = await fetch("http://localhost:8080/effect@3.19.16?exports=Effect", { headers: { "User-Agent": "i'm a browser" } });
-  assertEquals(res.status, 200);
-  const text = await res.text();
-  assert(!text.includes('import "'));
-  assertStringIncludes(text, 'export * from "/effect@3.19.16/es2022/effect.mjs?exports=Effect"');
+  {
+    const res = await fetch("http://localhost:8080/effect@3.19.16?exports=Effect", { headers: { "User-Agent": "i'm a browser" } });
+    assertEquals(res.status, 200);
+    const text = await res.text();
+    assert(!text.includes('import "'));
+    assertStringIncludes(text, 'export * from "/effect@3.19.16/es2022/effect.mjs?exports=Effect"');
+  }
+  {
+    const res = await fetch("http://localhost:8080/effect@3.19.16/es2022/effect.mjs?exports=Effect", {
+      headers: { "User-Agent": "i'm a browser" },
+    });
+    assertEquals(res.status, 200);
+    const text = await res.text();
+    assertEquals(text.trim(), 'import*as r from"./Effect.mjs";export{r as Effect};');
+  }
 });
