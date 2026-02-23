7066b5f7ea36e0d31c38ba657c110a2ed6d1078c
Je Xia <i@jex.me>
2026-02-23 20:10:51 +0800

    fix 'transform' testing

--- test/transform/transform.test.ts
+++ test/transform/transform.test.ts
@@ -1,50 +1,48 @@
 import { assertEquals, assertStringIncludes } from "jsr:@std/assert";
 
 Deno.test("transform API", async () => {
-  (async () => {
-    const options = {
-      lang: "jsx",
-      code: `
-        import { renderToString } from "preact-render-to-string";
-        export default () => renderToString(<h1>esm.sh</h1>);
-      `,
-      target: "es2022",
-      importMap: {
-        imports: {
-          "preact/jsx-runtime": "https://esm.sh/preact@10.13.2/jsx-runtime",
-          "preact-render-to-string": "https://esm.sh/preact-render-to-string6.0.2",
-        },
+  const options = {
+    lang: "jsx",
+    code: `
+      import { renderToString } from "preact-render-to-string";
+      export default () => renderToString(<h1>esm.sh</h1>);
+    `,
+    target: "es2022",
+    importMap: {
+      imports: {
+        "preact/jsx-runtime": "https://esm.sh/preact@10.13.2/jsx-runtime",
+        "preact-render-to-string": "https://esm.sh/preact-render-to-string6.0.2",
       },
-      sourceMap: "external",
-      minify: true,
-    };
-    const hash = await computeHash(
-      options.lang + options.code + options.target + JSON.stringify(options.importMap) + options.sourceMap + options.minify,
-    );
-    const res1 = await fetch("http://localhost:8080/transform", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(options),
-    });
-    assertEquals(res1.status, 200);
-    const transformOut = await res1.json();
-    assertStringIncludes(transformOut.code, `"preact/jsx-runtime"`);
-    assertStringIncludes(transformOut.code, `("h1"`);
-    assertStringIncludes(transformOut.code, `//# sourceMappingURL=+${hash}.mjs.map`);
-    assertStringIncludes(transformOut.map, `"mappings":`);
-
-    const res2 = await fetch(`http://localhost:8080/+${hash}.mjs`);
-    assertEquals(res2.status, 200);
-    assertEquals(res2.headers.get("Content-Type"), "application/javascript; charset=utf-8");
-    const js = await res2.text();
-    assertEquals(js, transformOut.code);
-
-    const res3 = await fetch(`http://localhost:8080/+${hash}.mjs.map`);
-    assertEquals(res3.status, 200);
-    assertEquals(res3.headers.get("Content-Type"), "application/json; charset=utf-8");
-    const map = await res3.text();
-    assertEquals(map, transformOut.map);
+    },
+    sourceMap: "external",
+    minify: true,
+  };
+  const hash = await computeHash(
+    options.lang + options.code + options.target + JSON.stringify(options.importMap) + options.sourceMap + options.minify,
+  );
+  const res1 = await fetch("http://localhost:8080/transform", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(options),
   });
+  assertEquals(res1.status, 200);
+  const transformOut = await res1.json();
+  assertStringIncludes(transformOut.code, `"preact/jsx-runtime"`);
+  assertStringIncludes(transformOut.code, `("h1"`);
+  assertStringIncludes(transformOut.code, `//# sourceMappingURL=+${hash}.mjs.map`);
+  assertStringIncludes(transformOut.map, `"mappings":`);
+
+  const res2 = await fetch(`http://localhost:8080/+${hash}.mjs`);
+  assertEquals(res2.status, 200);
+  assertEquals(res2.headers.get("Content-Type"), "application/javascript; charset=utf-8");
+  const js = await res2.text();
+  assertEquals(js, transformOut.code);
+
+  const res3 = await fetch(`http://localhost:8080/+${hash}.mjs.map`);
+  assertEquals(res3.status, 200);
+  assertEquals(res3.headers.get("Content-Type"), "application/json; charset=utf-8");
+  const map = await res3.text();
+  assertEquals(map, transformOut.map);
 });
 
 async function computeHash(input: string): Promise<string> {
