191eae47411810f42b744e54bbd37a2ecc247161
Je Xia <i@jex.me>
2026-02-24 16:59:07 +0800

    cli: support windows

--- cli/npm/install.mjs
+++ cli/npm/install.mjs
@@ -2,12 +2,14 @@ import { chmodSync, createWriteStream, existsSync, linkSync, readFileSync, statS
 import { createRequire } from "node:module";
 import { Writable } from "node:stream";
 
+const binExtension = process.platform === "win32" ? ".exe" : "";
+
 // 1. Attempt to resolve "@esm.sh/cli-{os}-{arch}", if not found, then try to download the binary from GitHub.
 // 2. On macOS/Linux, link "bin/esm.sh" to "@esm.sh/cli-{os}-{arch}/bin/esm.sh" if exists.
 install();
 
 async function install() {
-  const binPath = toPackagePath("bin/esm.sh" + getBinExtension());
+  const binPath = toPackagePath("bin/esm.sh" + binExtension);
   try {
     const nativeBinPath = resolveBinaryPath();
     if (process.platform !== "win32") {
@@ -29,8 +31,8 @@ async function install() {
 }
 
 function resolveBinaryPath() {
-  const cliBinPackage = `@esm.sh/cli-${getOS()}-${getArch()}`;
-  const binPath = createRequire(import.meta.url).resolve(cliBinPackage + "/bin/esm.sh" + getBinExtension());
+  const cliBinPackage = `@esm.sh/cli-${currentOS()}-${currentArch()}`;
+  const binPath = createRequire(import.meta.url).resolve(cliBinPackage + "/bin/esm.sh" + binExtension);
   if (!existsSync(binPath)) {
     throw new Error(`Could not find the binary of '${cliBinPackage}'`);
   }
@@ -41,7 +43,7 @@ async function downloadBinaryFromGitHub() {
   const pkgInfo = JSON.parse(readFileSync(toPackagePath("package.json"), "utf8"));
   const [_, minor, patch] = pkgInfo.version.split(".");
   const tag = "v" + minor + (Number(patch) > 0 ? "_" + patch : "");
-  const url = `https://github.com/esm-dev/esm.sh/releases/download/${tag}/cli-${getOS()}-${getArch()}${getBinExtension()}.gz`;
+  const url = `https://github.com/esm-dev/esm.sh/releases/download/${tag}/cli-${currentOS()}-${currentArch()}${binExtension}.gz`;
   const res = await fetch(url);
   if (!res.ok) {
     res.body?.cancel();
@@ -50,7 +52,7 @@ async function downloadBinaryFromGitHub() {
   return res.body.pipeThrough(new DecompressionStream("gzip"));
 }
 
-function getOS() {
+function currentOS() {
   switch (process.platform) {
     case "darwin":
       return "darwin";
@@ -63,7 +65,7 @@ function getOS() {
   }
 }
 
-function getArch() {
+function currentArch() {
   switch (process.arch) {
     case "arm64":
       return "arm64";
@@ -72,10 +74,6 @@ function getArch() {
     default:
       throw new Error(`Unsupported architecture: ${process.arch}`);
   }
-}
-
-function getBinExtension() {
-  return process.platform === "win32" ? ".exe" : "";
 }
 
 function toPackagePath(filename) {
